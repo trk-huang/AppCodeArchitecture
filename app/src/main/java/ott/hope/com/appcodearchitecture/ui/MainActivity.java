4a24fc31d8366fb8ff44692723f360f9c02e1060
trk-huang <sunhdj@163.com>
2016-06-17 11:55:24 +0800

    系统架构

--- app/src/main/java/ott/hope/com/appcodearchitecture/ui/MainActivity.java
+++ app/src/main/java/ott/hope/com/appcodearchitecture/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package ott.hope.com.appcodearchitecture;
+package ott.hope.com.appcodearchitecture.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ott.hope.com.appcodearchitecture.R;
 
 public class MainActivity extends AppCompatActivity {
 
